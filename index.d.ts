68061ad1b159fe70d434b80e2e615d26b4164a28
raohai.rh <raohai.rh@antgroup.com>
2022-03-16 23:35:35 +0800

    fix: module declares

--- index.d.ts
+++ index.d.ts
@@ -2,9 +2,13 @@ export interface YLRUOptions {
   maxAge?: number;
 }
 
-export default class YLRU {
+declare class YLRU {
   constructor(max: number)
   get: <T = any>(key: any, options?: YLRUOptions) => T | undefined;
   set: <T = any>(key: any, value: T, options?: YLRUOptions) => void;
   keys: () => any[];
+}
+
+declare module 'ylru' {
+  export = YLRU;
 }
